4a54f167a20e33a143fc3a9fecc182ac5dedad3c
JINN-OF-TG <84127769+JINN-OF-TG@users.noreply.github.com>
2021-05-13 16:15:09 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -161,7 +161,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                      [InlineKeyboardButton(text="🎬 Youtube Channel", url="https://www.youtube.com/stealthtechnogaming?sub_confirmation=1"), InlineKeyboardButton(text="🛠 Help", url="https://t.me/{}?start=help".format(bot.username)) ]]))
 
     else:
-        update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
+        update.effective_message.reply_text("YES I AM ALIVE🤔...")
 
 
 # for test purposes
